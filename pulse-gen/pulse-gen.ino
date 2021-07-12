d7d355e4b3f5fc35beefc30670e22875c49361de
Gabriel Wilson <47883480+g-i-wilson@users.noreply.github.com>
2021-07-12 12:47:36 -0500

    added LED blink for every 500ns pulse

--- pulse-gen/pulse-gen.ino
+++ pulse-gen/pulse-gen.ino
@@ -1,26 +1,29 @@
-int outputPin = 13;
-
-int initPrf = 100; // pulse repetition frequency [ms]; default is 100Hz
+int initDuration = 1000; // pulse duration [ms]
 
 void setup() {
-  pinMode(outputPin,OUTPUT);
+  pinMode(12,OUTPUT); // pulse (PB4)
+  pinMode(13,OUTPUT); // LED (PB5)
+
+  PORTB=0b11000000;
   
   Serial.begin(115200);
-  Serial.setTimeout(initPrf);
+  Serial.setTimeout(initDuration);
   
   while(!Serial){;}
 }
 
 void loop() {
 
-  int newPrf = Serial.parseInt();
-  if (newPrf>0) {
-    Serial.setTimeout(newPrf);
-    Serial.print("PRF set to: ");
-    Serial.println(newPrf);
+  int newDuration = Serial.parseInt();
+  if (newDuration>10) {
+    Serial.setTimeout(newDuration-10);
+    Serial.print("Duration [ms]: ");
+    Serial.println(newDuration);
   }
 
-  PORTB=11100000;
+  PORTB=0b11100000;
+  delay(10); // 1/100 second blink on
+  PORTB=0b11010000;
   __asm__("nop\n\t");
   __asm__("nop\n\t");
   __asm__("nop\n\t");
@@ -28,6 +31,6 @@ void loop() {
   __asm__("nop\n\t");
   __asm__("nop\n\t");
   __asm__("nop\n\t");
-  PORTB=11000000; // 8 clock cycles == 500ns
+  PORTB=0b11000000; // 8 clock cycles == 500ns
 
 }
